6db3b768ccf867c8180c307e827cf6f2e52d6d37
muhammad aji <95458296+achidoang@users.noreply.github.com>
2024-11-13 00:23:01 +0700

    danu malesan

--- src/models/User.js
+++ src/models/User.js
@@ -35,6 +35,52 @@ const User = sequelize.define("User", {
     type: DataTypes.ENUM("admin", "farmer"),
     defaultValue: "farmer",
   },
-});
+},
+// src/models/User.js
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/sequelize");
+
+const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  fullname: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  username: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  gender: {
+    type: DataTypes.ENUM("Male", "Female"),
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  telephone: DataTypes.STRING,
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.ENUM("admin", "farmer"),
+    defaultValue: "farmer",
+  },
+},{
+  tableName: "users",
+  timestamps: false, // Menonaktifkan createdAt dan updatedAt
+}
+);
+
+module.exports = User;
+
+);
 
 module.exports = User;
