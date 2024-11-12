4909ef9e1492c050e3621b03c9567a79f200408e
muhammad aji <95458296+achidoang@users.noreply.github.com>
2024-11-13 00:29:36 +0700

    wesss

--- src/models/User.js
+++ src/models/User.js
@@ -2,85 +2,46 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/sequelize");
 
-const User = sequelize.define("User", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    fullname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    gender: {
+      type: DataTypes.ENUM("Male", "Female"),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    telephone: DataTypes.STRING,
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM("admin", "farmer"),
+      defaultValue: "farmer",
+    },
   },
-  fullname: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  username: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false,
-  },
-  gender: {
-    type: DataTypes.ENUM("Male", "Female"),
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false,
-  },
-  telephone: DataTypes.STRING,
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  role: {
-    type: DataTypes.ENUM("admin", "farmer"),
-    defaultValue: "farmer",
-  },
-},
-// src/models/User.js
-const { DataTypes } = require("sequelize");
-const sequelize = require("../config/sequelize");
-
-const User = sequelize.define("User", {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  fullname: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  username: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false,
-  },
-  gender: {
-    type: DataTypes.ENUM("Male", "Female"),
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    unique: true,
-    allowNull: false,
-  },
-  telephone: DataTypes.STRING,
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  role: {
-    type: DataTypes.ENUM("admin", "farmer"),
-    defaultValue: "farmer",
-  },
-},{
-  tableName: "users",
-  timestamps: false, // Menonaktifkan createdAt dan updatedAt
-}
-);
-
-module.exports = User;
-
+  {
+    tableName: "user",
+    timestamps: false, // Menonaktifkan createdAt dan updatedAt
+  }
 );
 
 module.exports = User;
